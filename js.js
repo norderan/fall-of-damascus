f0db4a5be71299e75661a3d8f48749043b1b7380
Yazan Abu Queider <55917255+norderan@users.noreply.github.com>
2024-12-27 22:27:46 +0200

    Add files structure and map

---
+++ js.js
@@ -0,0 +1,10 @@
+
+
+//map intilization
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+//osm layer
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+osm.addTo(map);
