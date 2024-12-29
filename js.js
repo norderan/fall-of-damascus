0b5e0aeca2fe394b2961b3bf60711522b26243ca
Yazan Abu Queider <55917255+norderan@users.noreply.github.com>
2024-12-29 15:17:15 +0200

    1

--- js.js
+++ js.js
@@ -1,10 +1,21 @@
 
 
 //map intilization
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([34.80, 38.99], 7);
 
 //osm layer
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> '
 })
+
+
+//google layer
+googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+
 osm.addTo(map);
+
+L.geoJSON(syria1).addTo(map);
